ca9472784136ec66ee6634d96988ff69a60c7f2a
20jy0232 <20jy0232@jec.ac.jp>
2022-01-14 10:35:06 +0900

    1

---
+++ folder/mypage.php
@@ -0,0 +1,14 @@
+<?php
+$page_title = 'マイページ';
+require "./site/header.php";
+require "./site/banner.php";
+?>
+<?php $page_title = 'マイページ' ?>
+
+<div style="text-align:center">
+    <img src="./img/OK.png" width="500" height="300">
+</div>
+
+<?php
+require "./site/footer.php"
+?>
