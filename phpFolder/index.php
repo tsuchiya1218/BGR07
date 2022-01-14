827c4b1c604626730b4fe19c893d479f3a1196a8
20jy0232 <20jy0232@jec.ac.jp>
2022-01-14 10:13:46 +0900

    ディレクトリ変更

---
+++ phpFolder/index.php
@@ -0,0 +1,14 @@
+<?php
+$page_title = 'トップページ';
+require "./site/header.php";
+require "./site/banner.php";
+?>
+
+</br>
+<div style="text-align:center">
+    <img src="./img/TOP_iphone.jpg" width="500" height="300">
+</div>
+
+<?php
+require "./site/footer.php"
+?>
