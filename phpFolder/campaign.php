c0c2a04c5c8077f52b3a2ceb190bef8d078f3932
20jy0232 <20jy0232@jec.ac.jp>
2022-01-14 11:32:05 +0900

    campaign

--- phpFolder/campaign.php
+++ phpFolder/campaign.php
@@ -6,13 +6,35 @@ require "./common/banner.php";
 
 <div id="item" class="clearfix">
     ！キャンペーン中の商品を表示！
-    <div class="itemlist">
-        <p><a href="./detail.php"><img src="./img/case/case1.jpg" width="140" height="150"></a></p>
-        <dl>
-            <dt><a href="./detai    l.php">TEST.CASE</a></dt>
-            <dd>1000\</dd>
-        </dl>
-    </div>
+    <tr>
+        <td>
+            <div class="itemlist">
+                <p><a href="./detail.php"><img src="./img/case/case1.jpg" width="140" height="150"></a></p>
+                <dl>
+                    <dt><a href="./detai    l.php">TEST.CASE</a></dt>
+                    <dd>1000\</dd>
+                </dl>
+            </div>
+        </td>
+        <td>
+            <div class="itemlist">
+                <p><a href="./detail.php"><img src="./img/case/case1.jpg" width="140" height="150"></a></p>
+                <dl>
+                    <dt><a href="./detai    l.php">TEST.CASE</a></dt>
+                    <dd>1000\</dd>
+                </dl>
+            </div>
+        </td>
+        <td>
+            <div class="itemlist">
+                <p><a href="./detail.php"><img src="./img/case/case1.jpg" width="140" height="150"></a></p>
+                <dl>
+                    <dt><a href="./detai    l.php">TEST.CASE</a></dt>
+                    <dd>1000\</dd>
+                </dl>
+            </div>
+        </td>
+    </tr>
 </div>
 
 <?php
