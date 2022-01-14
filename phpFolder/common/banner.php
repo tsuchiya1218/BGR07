cebcfb245d7c442d3813e9b4297b24c11e6662b9
20jy0103 <20jy0103@jec.ac.jp>
2022-01-14 12:22:50 +0900

    push

--- phpFolder/common/banner.php
+++ phpFolder/common/banner.php
@@ -1,18 +1,21 @@
+<link rel="stylesheet" type="text/css" href="./css/banner.css">
 <!-- BannerIMG -->
 <h4 align = center>↓バナー部分↓</h4>
-<div style="text-align:center">
-    <a href="./index.php"><img src="./img/Banner.png" width="500" height="150"></a>
+<div id="banner" style="text-align:center">
+    <a href="./index.php">
+        <img src="./img/Banner.png" width="500" height="150">
+        <ul id="side">
+            <li><a href="./login.php">ログイン</a></li>
+            <li><a href="./register.php">無料会員登録</a></li>
+            <li><a href="./cart.php">カート</a></li>
+        </ul>
+    </a>
 </div>
-
-<a href="./login.php">ログイン</a>
-<a href="./register.php">無料会員登録</a>
-<a href="./cart.php">カート</a>
-
 
 </br>
 <!-- Menu -->
 <nav>
-    <div style="text-align:center">
+    <div id="menu" style="text-align:center">
         <a href="campaign.php">キャンペーン中</a> |
         <a href="">（カテゴリ等）</a> |
         <a href="">（ダミー）</a>
