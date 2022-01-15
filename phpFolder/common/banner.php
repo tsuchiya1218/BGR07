da7b2d844b76fd1455e3d993af5fc71a7fef5fdf
20jy0232 <20jy0232@jec.ac.jp>
2022-01-16 07:49:38 +0900

    id→class

--- phpFolder/common/banner.php
+++ phpFolder/common/banner.php
@@ -1,26 +1,32 @@
 <link rel="stylesheet" type="text/css" href="./css/banner.css">
-<!-- BannerIMG -->
-<h4 align = center>↓バナー部分↓</h4>
-<div id="banner" style="text-align:center">
-    <a href="./index.php">
-        <img src="./img/Banner.png" width="500" height="150">
-        <ul id="side">
+<!-- 消します -->
+<h4 align=center>↓バナー部分↓</h4>
+<header class="header">
+    <div class="banner">
+        <div class="header_brand">
+            <a href="./index.php">
+                <!-- BannerIMG -->
+                <img src="./img/Banner.png" wclassth="150" height="45">
+            </a>
+        </div>
+        <div class="header_spascer">
+        </div>
+        <ul class="side">
             <li><a href="./login.php">ログイン</a></li>
             <li><a href="./register_customer.php">無料会員登録</a></li>
             <li><a href="./cart.php">カート</a></li>
         </ul>
-    </a>
-</div>
-
-</br>
-<!-- Menu -->
-<nav>
-    <div id="menu" style="text-align:center">
-        <a href="campaign.php">キャンペーン中</a> |
-        <a href="">（カテゴリ等）</a> |
-        <a href="">（ダミー）</a>
-        <a href="">（ダミー）</a>
-        <a href="">（ダミー）</a>
     </div>
-</nav>
-<h4 align = center>↑バナー部分↑</h4>
+    </br>
+    <!-- Menu -->
+    <nav>
+        <div class="menu" style="text-align:center">
+            <a href="campaign.php">キャンペーン中</a> |
+            <a href="">（カテゴリ等）</a> |
+            <a href="">（ダミー）</a>
+            <a href="">（ダミー）</a>
+            <a href="">（ダミー）</a>
+        </div>
+    </nav>
+</header>
+<h4 align=center>↑バナー部分↑</h4>
