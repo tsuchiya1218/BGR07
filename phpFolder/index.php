41eb8a062ade8a91506ac0aa44b545fd10272f89
20jy0232 <20jy0232@jec.ac.jp>
2022-01-18 10:32:05 +0900

    index.css

--- phpFolder/index.php
+++ phpFolder/index.php
@@ -2,16 +2,19 @@
 require "./common/header.php";
 require "./common/banner.php";
 ?>
-<div class="item_flexbox">
+<link rel="stylesheet" href="./css/index.css">
 <?php
+echo "\n".'<div class="item_flexbox">'."\n";
 for ($i = 1; $i < 9; $i++) {
     echo <<<EOM
-    <div class="item_content">
-        さんぷる
-    </div>
+        <div class="item_content">
+            さんぷる$i<br>
+            <img src="./img/case/case1.jpg" width="200" height="200">
+        </div>\n
     EOM;
 }
 ?>
+</div>
 
 <?php
 require "./common/footer.php";
