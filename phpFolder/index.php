b213e157a16a42ec18595dde6d6533f2979dc4bf
20jy0232 <20jy0232@jec.ac.jp>
2022-01-19 06:52:07 +0900

    index

--- phpFolder/index.php
+++ phpFolder/index.php
@@ -5,7 +5,15 @@ require "./common/banner.php";
 <link rel="stylesheet" href="./css/index.css">
 <?php
 echo "\n".'<div class="item_flexbox">'."\n";
-for ($i = 1; $i < 9; $i++) {
+echo <<<EOM
+<div class="item_content">
+    さんぷる1(ここだけ画像リンク)<br>
+    <a href="./detail.php">
+        <img src="./img/case/case1.jpg" width="200" height="200">
+    </a>
+</div>\n
+EOM;
+for ($i = 2; $i < 10; $i++) {
     echo <<<EOM
         <div class="item_content">
             さんぷる$i<br>
