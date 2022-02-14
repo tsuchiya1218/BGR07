6f686a4d4f72d40c2eaf6bbe10df02f0c84a1047
20jy0103 <20jy0103@jec.ac.jp>
2022-02-14 16:31:54 +0900

    order js とりあえず

--- phpFolder/order_checkout.php
+++ phpFolder/order_checkout.php
@@ -2,11 +2,13 @@
 require "./common/header.php";
 require "./common/banner.php";
 ?>
+<script src="./js/order_confirm.js"></script>
+
 <p>支払い方法を選択してください</p>
 <form action="order_comp.php" method="POST">
     <input type="radio" name="checkout">クレジットカード決済<br>
     <input type="radio" name="checkout">コンビニ支払い<br>
-    <input type="submit" value="決済">
+    <button type="button" onClick="choice()">決済</button>
 </form>
 <?php
 require "./common/footer.php";
