f9b8a01e919f74957a3574f3516f411d4c255558
20jy0103 <20jy0103@jec.ac.jp>
2022-02-16 16:36:36 +0900

    order_address Sqlの情報取得

--- phpFolder/order_address.php
+++ phpFolder/order_address.php
@@ -1,20 +1,41 @@
 <?php
 require "./common/header.php";
 require "./common/banner.php";
+$CCode = 1;
 ?>
 <p>
     お届け先は以下の住所でよろしいですか？<br>
     変更する場合は変更ボタンを<br>
     よろしければお支払方法の選択に進みます
 </p>
-<table border="1">
+<?php
+
+    echo <<< EOM
+    <table border='1'>
     <tr>
         <th>郵便番号</th>
         <th>都道府県</th>
         <th>市区町村</th>
         <th>建物名</th>
         <th></th>
-    </tr>
+    </tr>\n
+    EOM;
+
+    $sql = "SELECT * FROM Customers WHERE CustomersCode = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($CCode));
+    $rec = $stmt->FETCH(PDO::FETCH_ASSOC);
+
+    {
+        echo "<tr><td class = >" . $rec['CustomersZip'] . "</td>";
+        echo "<td class = >" . $rec['CustomersAddress1'] . "</td>";
+        echo "<td class = >" . $rec['CustomersAddress2'] . "</td>";
+        echo "<td class = >" . $rec['CustomersAddress2'] . "</td>";
+        echo "<td><input type=\"button\" value=\"変更する\"></td></tr>\n";
+        
+    }
+?>
+
     <tr>
         <td>100-0001</td>
         <td>東京都</td>
