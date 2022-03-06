a90ffc8095ad5990bf053604cccfb08182364f21
20jy0232 <20jy0232@jec.ac.jp>
2022-03-07 04:35:03 +0900

    index.phpの手本作成

--- phpFolder/index.php
+++ phpFolder/index.php
@@ -6,9 +6,10 @@ require_once "./common/db_connect.php";
 
 ?>
 <link rel="stylesheet" href="./css/display_contents.css">
-<div class="item_flexbox">
-    <?php
-    $sql = "SELECT Goods.GoodsID,Goods.GoodsName,CategoryName,Price,ImgURL,ReviewCount,DisRatio,MoreThan,Goods.CampaignID
+<main>
+    <div class="item_flexbox">
+        <?php
+        $sql = "SELECT Goods.GoodsID,Goods.GoodsName,CategoryName,Price,ImgURL,ReviewCount,DisRatio,MoreThan,Goods.CampaignID
             FROM Goods
             INNER JOIN Category
             ON Goods.CategoryID = Category.CategoryID
@@ -17,11 +18,11 @@ require_once "./common/db_connect.php";
             INNER JOIN Campaign
             ON Goods.CampaignID = Campaign.CampaignID
             ORDER BY Goods.ReviewCount DESC";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    while (($rec = $stmt->FETCH(PDO::FETCH_ASSOC)) != null) {
-        if ($rec['CampaignID'] >= 200) {
-            echo <<<EOM
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        while (($rec = $stmt->FETCH(PDO::FETCH_ASSOC)) != null) {
+            if ($rec['CampaignID'] >= 200) {
+                echo <<<EOM
         <div class="item_content">
             <a href="./goods_detail.php?gID=$rec[GoodsID]">
             <div class="content_img">
@@ -30,35 +31,74 @@ require_once "./common/db_connect.php";
                     <img src="./img/sale.png" width="100px" height="100px">
                 </div>
             </div>
-            </a><br>
-            $rec[GoodsName]<br>
-            $rec[CategoryName]<br>
-            $rec[Price]<br>
-            $rec[DisRatio]%引き<br>
-            合計価格「$rec[MoreThan]」円以上で500円引き<br>
-            いいね数「$rec[ReviewCount]」
+            </a>
+            <div class="goods_name">
+                $rec[GoodsName]
+            </div>
+            $rec[CategoryName]
+            <div class="price_row">
+                <div class="price">
+                    ￥$rec[Price]
+                </div>
+                <div class="price_spacer">
+                </div>
+                <div class="dis_ratio">
+                    $rec[DisRatio]%引き
+                </div>
+            </div>
+            <div class="review_count">
+                いいね数「$rec[ReviewCount]」
+            </div>
         </div>\n
     EOM;
-        } else {
-            echo <<<EOM
+            } else if ($rec['MoreThan'] != null) {
+                echo <<<EOM
         <div class="item_content">
             <a href="./goods_detail.php?gID=$rec[GoodsID]">
             <div class="content_img">
                 <img src="./img/$rec[ImgURL]" width="200" height="200">
             </div>
-            </a><br>
-            $rec[GoodsName]<br>
-            $rec[CategoryName]<br>
-            $rec[Price]<br>
-            $rec[DisRatio]%引き<br>
-            合計価格「$rec[MoreThan]」円以上で500円引き<br>
-            いいね数「$rec[ReviewCount]」
+            </a>
+            <div class="goods_name">
+                $rec[GoodsName]
+            </div>
+            $rec[CategoryName]
+            <div class = "price_row">
+                ￥$rec[Price]
+            </div>
+            <div class="more_than">
+                合計価格「$rec[MoreThan]」円以上で500円引き
+            </div>
+            <div class="review_count">
+                いいね数「$rec[ReviewCount]」
+            </div>
         </div>\n
     EOM;
+            } else {
+                echo <<<EOM
+        <div class="item_content">
+            <a href="./goods_detail.php?gID=$rec[GoodsID]">
+            <div class="content_img">
+                <img src="./img/$rec[ImgURL]" width="200" height="200">
+            </div>
+            </a>
+            <div class="goods_name">
+                $rec[GoodsName]
+            </div>
+            $rec[CategoryName]
+            <div class = "price_row">
+                ￥$rec[Price]
+            </div>
+            <div class="review_count">
+                いいね数「$rec[ReviewCount]」
+            </div>
+        </div>\n
+    EOM;
+            }
         }
-    }
-    ?>
-</div>
+        ?>
+    </div>
+</main> 
 <?php
 require "./common/footer.php";
 ?>
