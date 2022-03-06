d1c3856c8f95d4560404b134fbf13e8016acff10
20jy0103 <20jy0103@jec.ac.jp>
2022-03-07 06:14:50 +0900

    自動付番、コード内内装

--- phpFolder/confirm_customer.php
+++ phpFolder/confirm_customer.php
@@ -3,16 +3,27 @@ session_start();
 require "./common/header.php";
 require "./common/banner.php";
 require_once "./common/db_connect.php";
-$cod = 100; //例
+$sql = "SELECT COUNT(*) as cnt
+        FROM Customers";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$rec = $stmt->FETCH(PDO::FETCH_ASSOC);
+$custcode = $rec['cnt'] + 1;
+
 $sql = "INSERT INTO Customers
         (CustomersCode, CustomersName, CustomersZip, CustomersAddress1, CustomersAddress2, CustomersPhone, CustomersEmail, CustomersPass)
         VALUES(?,?,?,?,?,?,?,?)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute(array(
-        $cod,
+        $custcode,
         $_SESSION['confirm']['name'], $_SESSION['confirm']['zipcode'],
         $_SESSION['confirm']['address1'], $_SESSION['confirm']['address2'],
         $_SESSION['confirm']['tel'], $_SESSION['confirm']['email'], $_SESSION['confirm']['pass']
 ));
-
+?>
+<h1 align="center">登録完了</h1>
+<br />
+<h3 align="center">登録完了しました。ホームページに戻ってログインしてください。</h3>
+<?php
 require "./common/footer.php";
+?>
