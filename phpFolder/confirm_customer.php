c812b817734ef81b58fda7dd5e9a7c59bcf3ac76
20jy0103 <20jy0103@jec.ac.jp>
2022-03-06 16:57:55 +0900

    新規登録 SESSION修正

--- phpFolder/confirm_customer.php
+++ phpFolder/confirm_customer.php
@@ -4,18 +4,12 @@ require "./common/header.php";
 require "./common/banner.php";
 require_once "./common/db_connect.php";
 
-$name = $_SESSION['name'];
-$zipcode = $_SESSION['zipcode'];
-$address1 = $_SESSION['address1'];
-$address2 = $_SESSION['address2'];
-$tel = $_SESSION['tel'];
-$email = $_SESSION['email'];
-$pass = $_SESSION['pass'];
-
 $sql = "INSERT INTO Customers
-        (CustomersName, CustomersZip, CustomersAddress1, CustomersAddress2, CustomersPhone, CustomersEmail, CustomersPass)
-        VALUES(?,?,?,?,?,?,?)";
+        (CustomersCode, CustomersName, CustomersZip, CustomersAddress1, CustomersAddress2, CustomersPhone, CustomersEmail, CustomersPass)
+        VALUES(100,?,?,?,?,?,?,?)";
 $stmt = $pdo->prepare($sql);
-$stmt->execute(array($name, $zipcode, $address1, $address2, $tel, $email, $pass));
+$stmt->execute(array($_SESSION['confirm']['name'], $_SESSION['confirm']['zipcode'], 
+                $_SESSION['confirm']['address1'], $_SESSION['confirm']['address2'], 
+                $_SESSION['confirm']['tel'], $_SESSION['confirm']['email'], $_SESSION['confirm']['pass']));
 
 require "./common/footer.php";
