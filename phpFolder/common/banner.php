02576a9a0ec15a45d4a8a66ca849964d6be55c0a
20jy0232 <20jy0232@jec.ac.jp>
2022-03-06 20:32:50 +0900

    飯食う前、同期

--- phpFolder/common/banner.php
+++ phpFolder/common/banner.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="./css/common.css">
 <link rel="stylesheet" type="text/css" href="./css/banner.css">
 <header class="header">
     <div class="header_inner">
--- phpFolder/goods_detail.php
+++ phpFolder/goods_detail.php
@@ -24,79 +24,90 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute(array($gID));
 $rec = $stmt->FETCH(PDO::FETCH_ASSOC)
 ?>
-<div class="goods_left">
-    <img src="./img/<?= $rec["ImgURL"] ?>" width="280" height="300">
-    <br>↓ここに別の画像を数枚表示↓<br>
-    　□　□　□　□<br>
-    ↑ここに別の画像を数枚表示↑
+<div class="page_body">
+    <div class="goods_left">
+        <img src="./img/<?= $rec["ImgURL"] ?>" width="280" height="300">
+        <br>↓ここに別の画像を数枚表示↓<br>
+        　□　□　□　□<br>
+        ↑ここに別の画像を数枚表示↑
 
-</div>
+    </div>
 
-<div class="goods_right">
-    <h2><?= $rec["GoodsName"] ?></h2>
-    <span class="price">
-        <h3>￥<?= $rec["Price"] ?></h3>
-    </span>
-    <form action="./review_check.php" method="GET">
-        <input type="hidden" name="gID" value="<?= $gID ?>">
-        <input type="submit" name="review" value="♡">
-    </form>
-    <?php
-    if (isset($_SESSION['eMsg'])) {
-        echo $_SESSION['eMsg'];
-        unset($_SESSION['eMsg']);
-    }
-    if (isset($_SESSION['Msg'])) {
-        echo $_SESSION['Msg'];
-        unset($_SESSION['Msg']);
-    }
-    
-    ?>
-    <table>
-        <tr>
-            <td>機種</td>
-            <td>iPhone7</td>
-        </tr>
-        <tr>
-            <td>色</td>
-            <td><?= $rec["ColorName"] ?></td>
-        </tr>
-        <tr>
-            <td>カテゴリ</td>
-            <td><?= $rec["CategoryName"] ?></td>
-        </tr>
-    </table>
-    <form action="check_cart_login.php" method="POST">
-        <select name="handQty">
-            <?php
-            for ($i = 1; $i <= 10; $i++) {
-                if ($i == 1) {
-                    echo "<option value=$i>{$i}個</option>\n";
-                }
-                echo "            <option value=$i>{$i}個</option>\n";
-            }
-            echo <<< EOM
+    <div class="goods_right">
+        <h2><?= $rec["GoodsName"] ?></h2>
+        <div class="price_row">
+            <h3>￥<?= $rec["Price"] ?></h3>
+            <div class="review_spacer">
+            </div>
+            <div class="review_button">
+                <form action="./review_check.php" method="GET">
+                    <input type="hidden" name="gID" value="<?= $gID ?>">
+                    <input type="submit" name="review" value="♡">
+                </form>
+            </div>
+            <div class="review_count">
+                <?= $rec["ReviewCount"] ?>
+            </div>
+        </div>
+
+        <?php
+        if (isset($_SESSION['eMsg'])) {
+            echo $_SESSION['eMsg'];
+            unset($_SESSION['eMsg']);
+        }
+        if (isset($_SESSION['Msg'])) {
+            echo $_SESSION['Msg'];
+            unset($_SESSION['Msg']);
+        }
+
+        ?>
+        <table>
+            <tr>
+                <th>機種：</th>
+                <td>iPhone7</td>
+            </tr>
+            <tr>
+                <th>色：</th>
+                <td><?= $rec["ColorName"] ?></td>
+            </tr>
+            <tr>
+                <th>カテゴリ：</th>
+                <td><?= $rec["CategoryName"] ?></td>
+            </tr>
+        </table>
+        <div class="qty_row">
+            <form action="check_cart_login.php" method="POST">
+                <select name="handQty">
+                    <?php
+                    for ($i = 1; $i <= 10; $i++) {
+                        if ($i == 1) {
+                            echo "<option value=$i>{$i}個</option>\n";
+                        }
+                        echo "            <option value=$i>{$i}個</option>\n";
+                    }
+                    echo <<< EOM
                         </select>
                             <input type="hidden" name="handGID" value=$gID>
                             <input type="submit" value="カートに入れる">
                 EOM;
-            if (isset($_SESSION['eMsg'])) {
-                echo $_SESSION['eMsg'];
-                unset($_SESSION['eMsg']);
-            }
-            ?>
-    </form>
-    <h4>説明</h4>
-    <p><?= $rec["GoodsExplanation"] ?></p>
+                    if (isset($_SESSION['eMsg'])) {
+                        echo $_SESSION['eMsg'];
+                        unset($_SESSION['eMsg']);
+                    }
+                    ?>
+            </form>
+        </div>
+        <div class="explanation">
+            <h4>説明</h4>
+            <p><?= $rec["GoodsExplanation"] ?></p>
+        </div>
+    </div>
+    <h4>注意事項</h4>
+    <ul>
+        <li>画像は合成イメージです。実際のアイテムとは異なる場合があります。</li>
+        <li>ご使用のモニターの設定により、実際のアイテムと色味が異なる場合があります。</li>
+    </ul>
 </div>
-<h4>注意事項</h4>
-<ul>
-    <li>画像は合成イメージです。実際のアイテムとは異なる場合があります。</li>
-    <li>ご使用のモニターの設定により、実際のアイテムと色味が異なる場合があります。</li>
-</ul>
-<h4>レビュー数</h4>
-<p><?= $rec["ReviewCount"] ?></p>
-
 <?php
 require "./common/footer.php"
 ?>
